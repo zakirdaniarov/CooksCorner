7d11d54b4cd2cc42c84357913dbe668b00dc1179
zakir <zakir95olymp@gmail.com>
2024-03-20 18:00:46 +0900

    updated settings.py in the main

--- main/settings.py
+++ main/settings.py
@@ -43,7 +43,6 @@ CORS_ALLOWED_ORIGINS = (
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://localhost:5173',
                         'http://localhost:8000', 'https://muha-backender.org.kg', 'https://neo-cooks.vercel.app']
 # Application definition
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 INSTALLED_APPS = [
